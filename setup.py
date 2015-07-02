1b2218ad25133f3fbe6d2001f6fc264cdc8dc1e1
Roman Komkov <roman.komkov@glogster.com>
2015-07-02 19:05:01 +0200

    Simple requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from pip.req import parse_requirements
 from os.path import dirname, join
 
 project_dir = dirname(__file__)
-install_reqs = parse_requirements(join(project_dir, 'requirements.txt'))
+install_reqs = parse_requirements(join(project_dir, 'requirements.txt'), session=False)
 
 setup(
     name='tintin',
-    version='alpha',
+    version='0.0.1',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    requires=['requests', 'simplejson', 'bencode']
+    install_requires=[str(ir.req) for ir in install_reqs],
 )
